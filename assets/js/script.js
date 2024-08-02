fe92162d2d9eb9ca197830b5af10d6fec4a0d682
Nicolás San Martin <nicolas.sm.lor@gmail.com>
2024-08-02 16:11:58 -0400

    mini desafio control de sticker

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,13 @@
 hasBorder = false;
+totalSticker = 0;
+stickerRainbow = 0;
+stickerDog = 0;
+stickerPizza = 0;
 
 yawningCatImg = document.querySelector("#yawningCatId");
+
+totalStickerSpan = document.querySelector("#totalStickerId");
+totalStickerSpan.innerHTML = totalSticker;
 
 const validateBorder = () => {
   if (hasBorder) {
@@ -9,5 +16,28 @@ const validateBorder = () => {
   } else {
     hasBorder = true;
     yawningCatImg.classList.add("border-img-yawning-cat");
+  }
+};
+
+const checkSticker = () => {
+  let totalStickerFinal = document.querySelector("#totalStickerId");
+  let totalStickerRainbowFinal = document.querySelector("#stickerRainbowId");
+  let totalStickerDogFinal = document.querySelector("#stickerDogId");
+  let totalStickerPizzaFinal = document.querySelector("#stickerPizzaId");
+
+  let contentTotalStickerFinal = Number(totalStickerFinal.innerHTML);
+  let contentTotalStickerRainbowFinal = Number(totalStickerRainbowFinal.value);
+  let contentTotalStickerDogFinal = Number(totalStickerDogFinal.value);
+  let contentTotalStickerPizzaFinal = Number(totalStickerPizzaFinal.value);
+
+  contentTotalStickerFinal =
+    contentTotalStickerRainbowFinal +
+    contentTotalStickerDogFinal +
+    contentTotalStickerPizzaFinal;
+
+  if (contentTotalStickerFinal <= 10) {
+    totalStickerFinal.innerHTML = contentTotalStickerFinal;
+  } else {
+    totalStickerFinal.innerHTML = "demasiados";
   }
 };
